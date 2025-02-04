d047940e4ddac5ee5157f062cfc8402e72837124
Ping <ping@alpacadb.com>
2025-02-04 19:40:10 +0900

    add market holidays and tests

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # market_holidays
+
 Custom maintained market holidays.
+
+## References
+
+XNYS Holidays: https://www.nyse.com/markets/hours-calendars
+XTKS Holidays: https://www.jpx.co.jp/english/corporate/about-jpx/calendar/
+Spring & Autumn Equinox Day prediction: https://www.nao.ac.jp/faq/a0301.html
---
+++ tests/test_market_holidays.py
@@ -0,0 +1,21 @@
+import polars as pl
+import json
+
+expected_calendars = ["XNYS"]
+
+
+def test_market_holidays_file():
+    data: dict = {}
+    with open("src/market_holidays.json") as file:
+        data = json.load(file)
+
+    for cal in expected_calendars:
+        assert cal in data, f"Missing {cal} in market_holidays.json file"
+        assert len(data[cal]) > 0
+
+        df = pl.DataFrame({"holiday": data[cal]})
+        assert df["holiday"].is_sorted(descending=False)  # assert increasing
+        assert df["holiday"].is_unique().all()  # assert no duplicates
+
+        parsed_dates = df["holiday"].str.strptime(pl.Date, "%Y-%m-%d", strict=True)
+        assert parsed_dates.is_not_null().all()  # assert all values are %Y%-%m-%d
